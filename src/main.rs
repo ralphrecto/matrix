830cddaf90a40aed4ba9fd4eec95a4677a51e9d4
Ralph Recto <recto.ralph@gmail.com>
2021-02-07 00:38:18 -0500

    Add some impls for struct method namespacing

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,11 @@ struct Color {
     b: u8 
 }
 
+impl Color {
+    const PURE_GREEN: Color = Color { r: 0, g: 5, b: 0};
+    const DARK_GREEN: Color = Color { r: 0, g: 1, b: 0 };
+}
+
 // TermPos is a 1-indexed character cell in the Term.
 #[derive(Debug, Clone, Copy)]
 struct TermPos {
@@ -44,47 +49,92 @@ struct Trail {
     len: usize,
     speed: i32
 }
+
+impl Trail {
+    const MAX_LEN: usize = 12;
+    const MAX_SPEED: i32 = 3;
+
+    fn new(x: u8, y: u8, len: usize, speed: i32) -> Trail {
+        return Trail {
+            bottom: TermPos {
+                x: x,
+                y: y
+            },
+            speed: speed,
+            len: len
+        };
+    }
+
+    fn random(term_size: (u16, u16)) -> Trail {
+        let x = thread_rng().gen_range(1..term_size.0);
+        let y = thread_rng().gen_range(1..term_size.1);
+        let len = thread_rng().gen_range(3..Trail::MAX_LEN);
+        let speed = thread_rng().gen_range(1..Trail::MAX_SPEED);
+
+        return Trail::new(x as u8, y as u8, len, speed);
+    }
+
+    fn is_visible(self: &Self, term_size: (u16, u16)) -> bool {
+        let top = self.bottom.y as i32 - self.len as i32;
+        return top < term_size.1 as i32;
+    }
+
+    const RAIN_CHARSET: &'static [char] = &[
+        'x', 'A', 'z', 'O',
+        '\u{00D8}', '\u{01C2}', '\u{03A9}', '\u{01E3}', '\u{03FC}',
+        '\u{305B}', '\u{3091}'
+    ];
+
+    fn gen_char() -> char {
+        return Trail::RAIN_CHARSET[thread_rng().gen_range(0..Trail::RAIN_CHARSET.len())];
+    }
+
+    fn render(self: &Self, stdout: &mut RawTerminal<Stdout>) -> Result<(), Error> {
+        let interpolates: Vec<Color> = interpolate(Color::PURE_GREEN, Color::DARK_GREEN, self.len as u8);
+
+        for i in 0..self.len {
+            let y = (self.bottom.y as i32) - (i as i32);
+            let x = self.bottom.x; 
+            let color: Color = interpolates[i];
+
+            if y < 1 {
+                continue;
+            }
+
+            write!(
+                stdout,
+                "{}{}{}",
+                cursor::Goto(x as u16, y as u16),
+                color::Fg(color::AnsiValue::rgb(color.r, color.g, color.b)),
+                Trail::gen_char()
+            );
+            stdout.flush()?;
+        }
+
+        return Ok(());
+    }
+}
+
 struct State {
     trails: Vec<Trail>,
     term_size: (u16, u16),
     num_trails: u32
 }
 
-fn new_trail(x: u8, y: u8, len: usize, speed: i32) -> Trail {
-    return Trail {
-        bottom: TermPos {
-            x: x,
-            y: y
-        },
-        speed: speed,
-        len: len
-    };
-}
+impl State {
+    fn new(term_size: (u16, u16), num_trails: u32) -> State {
+        let mut trails: Vec<Trail> = vec![];
 
-const MAX_TRAIL_LEN: usize = 12;
-const MAX_TRAIL_SPEED: i32 = 3;
+        for i in 0..num_trails {
+            trails.push(Trail::random(term_size));
+        }
 
-fn new_random_trail(term_size: (u16, u16)) -> Trail {
-    let x = thread_rng().gen_range(1..term_size.0);
-    let y = thread_rng().gen_range(1..term_size.1);
-    let len = thread_rng().gen_range(3..MAX_TRAIL_LEN);
-    let speed = thread_rng().gen_range(1..MAX_TRAIL_SPEED);
-
-    return new_trail(x as u8, y as u8, len, speed);
-}
-
-fn new_state(term_size: (u16, u16), num_trails: u32) -> State {
-    let mut trails: Vec<Trail> = vec![];
-
-    for i in 0..num_trails {
-        trails.push(new_random_trail(term_size));
+        return State {
+            trails: trails,
+            term_size: term_size,
+            num_trails: num_trails
+        };
     }
-
-    return State {
-        trails: trails,
-        term_size: term_size,
-        num_trails: num_trails
-    };
 }
 
 fn compute_step_size(c1: u8, c2: u8, steps: u8) -> i32 {
@@ -128,16 +178,11 @@ fn interpolate(c1: Color, c2: Color, steps: u8) -> Vec<Color> {
     return interpolates;
 }
 
-fn is_visible(trail: &Trail, term_size: (u16, u16)) -> bool {
-    let top = trail.bottom.y as i32 - trail.len as i32;
-    return top < term_size.1 as i32;
-}
-
 fn tick(mut state: &mut State) {
     // Replace trails if they are no longer visible.
     for i in 0..state.trails.len() {
-        if !is_visible(&state.trails[i], state.term_size) {
-            (&mut state).trails[i] = new_random_trail(state.term_size);
+        if !state.trails[i].is_visible(state.term_size) {
+            (&mut state).trails[i] = Trail::random(state.term_size);
         }
     }
 
@@ -147,49 +192,10 @@ fn tick(mut state: &mut State) {
     }
 }
 
-const PURE_GREEN: Color = Color { r: 0, g: 5, b: 0};
-const DARK_GREEN: Color = Color { r: 0, g: 1, b: 0 };
-
-const RAIN_CHARSET: &'static [char] = &[
-    'x', 'A', 'z', 'O',
-    '\u{00D8}', '\u{01C2}', '\u{03A9}', '\u{01E3}', '\u{03FC}',
-    '\u{305B}', '\u{3091}'
-];
-
-fn gen_char() -> char {
-    return RAIN_CHARSET[thread_rng().gen_range(0..RAIN_CHARSET.len())];
-}
-
-fn render_trail(stdout: &mut RawTerminal<Stdout>, trail: &Trail) -> Result<(), Error> {
-    let interpolates: Vec<Color> = interpolate(PURE_GREEN, DARK_GREEN, trail.len as u8);
-
-    for i in 0..trail.len {
-        let y = (trail.bottom.y as i32) - (i as i32);
-        let x = trail.bottom.x; 
-        let color: Color = interpolates[i];
-
-        if y < 1 {
-            continue;
-        }
-
-        write!(
-            stdout,
-            "{}{}{}",
-            cursor::Goto(x as u16, y as u16),
-            color::Fg(color::AnsiValue::rgb(color.r, color.g, color.b)),
-            gen_char()
-        );
-        stdout.flush()?;
-    }
-
-    return Ok(());
-}
-
-
 fn render(mut stdout: &mut RawTerminal<Stdout>, state: &State) {
     write!(stdout, "{}", clear::All);
     for trail in &state.trails {
-        render_trail(&mut stdout, trail);
+        trail.render(&mut stdout);
     }
 }
 
@@ -203,23 +209,30 @@ fn read_key(mut stdin: &mut AsyncReader) -> Option<u8> {
     }
 }
 
+fn clear_screen(mut stdout: &mut RawTerminal<Stdout>) -> Result<(), Error>  {
+    write!(stdout, "{}{}{}", clear::All, cursor::Goto(1,1), color::Fg(color::Reset));
+    return stdout.flush();
+}
+
+// Will render 1 trail per $TRAIL_DENSITY terminal squares.
 const DEFAULT_TRAIL_DENSITY: u32 = 30;
 
 fn main() -> Result<(), Error> {
+    // Set up stdin/stdout.
     let mut stdin = async_stdin();
     let mut stdout = stdout().into_raw_mode()?;
+
+    // Set up data.
     let term_size: (u16, u16) = match terminal_size() {
         Ok(size) => size,
         Err(_) => panic!("cannot get term size")
     };
 
     let num_trails = (term_size.0 as i32 * term_size.1 as i32) as i32 / DEFAULT_TRAIL_DENSITY as i32;
+    let mut state: State = State::new(term_size, num_trails as u32);
 
-    let mut state: State = new_state(term_size, num_trails as u32);
-
-    write!(stdout, "{}{}{}{}", clear::All, cursor::Goto(1,1), color::Fg(color::Reset), cursor::Hide);
-    stdout.flush()?;
-
+    // Enter main loop.
+    clear_screen(&mut stdout)?;
     loop {
         tick(&mut state);
         render(&mut stdout, &state);
@@ -234,9 +247,7 @@ fn main() -> Result<(), Error> {
 
         thread::sleep(time::Duration::from_millis(150));
     }
-
-    write!(stdout, "{}{}{}", clear::All, cursor::Goto(1,1), color::Fg(color::Reset));
-    stdout.flush()?;
+    clear_screen(&mut stdout)?;
 
     return Ok(());
 }
